242095f1e81cecd976f874cdbfed59d6b0d5ad92
Ahmed ASMANE <aasmane@neosys.fr>
2019-04-08 14:03:00 +0100

    Mofication CrowlerServices

--- Crowler.API/Services/CrowlerService.cs
+++ Crowler.API/Services/CrowlerService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Crowler.API.Services
 {
     /// <summary>
-    /// Crowling Class
+    /// Crowling Class Services
     /// </summary>
     public class CrowlerService : ICrowlerService
     {
